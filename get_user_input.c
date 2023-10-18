04fafb3e83ceac693956a0612e7c5235fe7ea87a
Yasmineltair <yasmineltair293@gmail.com>
2023-10-18 02:38:10 -0700

    update for betty

--- get_user_input.c
+++ get_user_input.c
@@ -11,11 +11,10 @@ char *get_user_input(void)
 	ssize_t nchar_read;
 
 	nchar_read = getline(&buf, &n, stdin);
-	
 	if (nchar_read == -1 || _strcmp(buf, "exit\n") == 0)
 	{
 		free(buf);
-		exit (1);
+		exit(1);
 	}
 	return (buf);
 }
