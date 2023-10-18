b899f203759674e08f04aa38fc55afc2db317901
khaledolemy <khaledolemy@gmail.com>
2023-10-18 04:08:51 +0300

    allowed functions

--- pre_cmd.c
+++ pre_cmd.c
@@ -9,14 +9,7 @@ int pre_line(void)
 {
 	if (isatty(STDIN_FILENO))
 	{
-		char cwd[1024];
-		char *user;
-		char host[1024];
-
-		user = getlogin();
-		getcwd(cwd, sizeof(cwd));
-		gethostname(host, sizeof(host));
-		_printf("%s@%s:%s$ ", user, host, cwd);
+		_printf("$ ");
 	}
 	return (0);
 }
